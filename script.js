3bce84bd1e563efaa41c97300b712036ef0d829e
Komal Meena <80938016+komalmeena6516@users.noreply.github.com>
2023-03-02 20:11:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const yourArray = [
-	[{name:"komal", age:20, female:true}],
+	[{name:"hello", age:20, female:true}],
 	[{name:"ram", age:20, female:false}],
 	[{name:"shyam", age:20, female:true}],
 	[{name:"dham", age:20, female:true}],
 	[{name:"kam", age:20, female:true}]
-]
+];
+console.log(yourArray);
